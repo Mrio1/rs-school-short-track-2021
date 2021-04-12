ccd61159a9c4fad1642ae902e7c7882848dc860c
A. Zhur <sanchesmrio1@gmail.com>
2021-04-13 01:34:18 +0300

    feat: solve 12th task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let curLet = str[0] || '';
+  let counter = 0;
+  const retArr = [];
+  [...str].forEach((item) => {
+    if (item === curLet) {
+      counter++;
+    } else {
+      retArr.push(counter !== 1 ? `${counter}${curLet}` : `${curLet}`);
+      curLet = item;
+      counter = 1;
+    }
+  });
+  retArr.push(counter > 1 ? `${counter}${curLet}` : `${curLet}`);
+  return retArr.join('');
 }
 
 module.exports = encodeLine;
